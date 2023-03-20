a51f1938ac25f4370c77631f78029cb9b1a02cbf
Mukesh-Khemani <mukeshkhemani222@gmail.com>
2023-03-20 23:46:09 +0530

    Add Header Elements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import "./App.css"
 import Login from './components/Login'
+import Header from './components/Header'
+import Home from './components/Home'
 function App() {
   return (
       <Router>
         <Routes>
           <Route path="/" element={ <Login/> } />
-         </Routes>
+          <Route path='/home' element={<><Header/><Home/></>}/>
+        </Routes>
       </Router>
   );
 }
